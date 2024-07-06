9480cccfbab2e16a4480b96a4585aaaf13abc94b
Yair Guadarrama <yair.g.guadarrama@icloud.com>
2024-07-05 18:20:54 -0600

    [FIX]Logic

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,14 +4,14 @@ import MainContent from "@/components/MainContent";
 import RightAside from "@/components/RightAside";
 import { getPosts, getUsers } from "@/lib/api";
 
-export default function Home({ posts }) {
+export default function Home({ posts, users }) {
 
-
+  
   return (
     <main className="grid grid-cols-1 md:grid-cols-[1fr_3fr] lg:grid-cols-[1fr_3fr_1.5fr] grid-rows-[55px_1fr]  bg-[#F5F5F5] h-lvh">
       <Navbar />
       <LeftAside />
-      <MainContent posts={posts} />
+      <MainContent posts={posts} users={users} />
       <RightAside/>
     </main>
   );
@@ -20,18 +20,21 @@ export default function Home({ posts }) {
 export async function getServerSideProps() {
   try {
     const posts = await getPosts();
+    const users = await getUsers();
 
     return {
       props: {
         posts,
+        users: users || []
       },
     };
   } catch (error) {
-    console.error(error);
+    console.error('Error fetching data:', error);
 
     return {
       props: {
         posts: [],
+        users: [],
       },
     };
   }
